3d146d6ed0f3fa2f1cd89e5cd35a45f0478a56d6
Jenny <jenny@simplemama.com>
2013-11-14 14:25:04 -0500

    minor description change (no v increment) + added pl:true

--- dms-breadcrumb-navxt.php
+++ dms-breadcrumb-navxt.php
@@ -3,9 +3,11 @@
 	Plugin Name: DMS Breadcrumb NavXT
 	Author: Simple Mama
 	Author URI: http://www.simplemama.com
-	Description: Display post and page breadcrumbs for reader and SEO benefit. This section requires that the Breadcrumb NavXT Wordpress plugin be installed and creates a seamless integration with PageLines DMS.
+	Description: Display post and page breadcrumbs for reader and SEO benefit. This section requires that the Breadcrumb NavXT Wordpress plugin be installed and creates a seamless integration with PageLines DMS. For PageLines DMS only.
 	Class Name: BreadcrumbNavXT
 	Demo:
 	Version: 1.0
+	PageLines: true
+    v3: true
 	Filter: nav
 */
--- sections/dms-breadcrumb-navxt/section.php
+++ sections/dms-breadcrumb-navxt/section.php
@@ -3,7 +3,7 @@
 	Section: DMS Breadcrumb NavXT
 	Author: Simple Mama
 	Author URI: http://www.simplemama.com
-	Description: Display post and page breadcrumbs for reader and SEO benefit. This section requires that the Breadcrumb NavXT Wordpress plugin be installed and creates a seamless integration with PageLines DMS.
+	Description: Display post and page breadcrumbs for reader and SEO benefit. This section requires that the Breadcrumb NavXT Wordpress plugin be installed and creates a seamless integration with PageLines DMS. For PageLines DMS only.
 	Class Name: BreadcrumbNavXT
 	Demo:
 	Version: 1.0
